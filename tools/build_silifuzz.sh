cce3584eca4517bbaedfb6ddf38b1927ee9422df
maknee <zhu109@illinois.edu>
2022-09-22 23:44:08 -0500

    Added silifuzz default corpus (inc eax example)

--- tools/build_silifuzz.sh
+++ tools/build_silifuzz.sh
@@ -7,7 +7,8 @@ cd silifuzz
 SILIFUZZ_SRC_DIR=`pwd`
 sudo ./install_build_dependencies.sh  # Currently, works for the latest Debian and Ubuntu only
 bazel build -c opt @silifuzz//tools:{snap_corpus_tool,fuzz_filter_tool,snap_tool,silifuzz_platform_id}
-bazel build -c debug @silifuzz//runner:reading_runner_main_nolibc @silifuzz//orchestrator:silifuzz_orchestrator_main
+bazel build -c opt @silifuzz//runner:reading_runner_main_nolibc
+bazel build -c opt @silifuzz//orchestrator:silifuzz_orchestrator_main
 SILIFUZZ_BIN_DIR=`pwd`/bazel-bin/
 cd "${SILIFUZZ_BIN_DIR}"
 
@@ -15,4 +16,5 @@ cd "${SILIFUZZ_SRC_DIR}"
 bazel build -c opt @silifuzz//proxies:unicorn_x86_64_sancov
 bazel build -c opt @centipede//:centipede
 
+cp ../silifuzz.corpus.xz $SILIFUZZ_BIN_DIR
 
