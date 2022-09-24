d5eeae6964eb71748e6f7ba13d0d96d80112cb86
maknee <zhu109@illinois.edu>
2022-09-24 01:15:24 -0500

    Updated build silifuzz to have independent builds

--- tools/build_silifuzz.sh
+++ tools/build_silifuzz.sh
@@ -6,7 +6,11 @@ git clone https://github.com/google/silifuzz.git
 cd silifuzz
 SILIFUZZ_SRC_DIR=`pwd`
 sudo ./install_build_dependencies.sh  # Currently, works for the latest Debian and Ubuntu only
-bazel build -c opt @silifuzz//tools:{snap_corpus_tool,fuzz_filter_tool,snap_tool,silifuzz_platform_id}
+#bazel build -c opt @silifuzz//tools:{snap_corpus_tool,fuzz_filter_tool,snap_tool,silifuzz_platform_id}
+bazel build -c opt @silifuzz//tools:snap_corpus_tool
+bazel build -c opt @silifuzz//tools:fuzz_filter_tool
+bazel build -c opt @silifuzz//tools:snap_tool
+bazel build -c opt @silifuzz//tools:silifuzz_platform_id
 bazel build -c opt @silifuzz//runner:reading_runner_main_nolibc
 bazel build -c opt @silifuzz//orchestrator:silifuzz_orchestrator_main
 SILIFUZZ_BIN_DIR=`pwd`/bazel-bin/
