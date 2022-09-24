92c7583673ca4aeacc3df9841713bf64bbeb999d
maknee <zhu109@illinois.edu>
2022-09-24 01:59:19 -0500

    added save corpus

--- tools/silifuzz_tools/generate_silifuzz_corpus.py
+++ tools/silifuzz_tools/generate_silifuzz_corpus.py
@@ -7,6 +7,8 @@ import logging
 import os
 from pathlib import Path
 import shutil
+import hashlib
+
 
 parser = argparse.ArgumentParser(description='Generates silifuzz corpus')
 parser.add_argument('--silifuzz_bin_dir', type=str, help='Silifuzz binary directory (to run Centipede fuzzer and unicorn binaries', default='tools/silifuzz')
@@ -15,6 +17,7 @@ parser.add_argument('--num_runs', type=int, help='Number of runs', default=10000
 parser.add_argument('--j', type=int, help='Number of cores', default=os.cpu_count())
 parser.add_argument('--work_dir', type=str, help='Work dir', default='tools/silifuzz_work_dir')
 parser.add_argument('--corpus_output', type=str, help='Corpus file generated name', default='tools/silifuzz.corpus')
+parser.add_argument('--corpus_save_dir', type=str, help='Corpus file generated name', default='../saved_corpus')
 args = parser.parse_args()
 
 def panic(s):
@@ -44,9 +47,25 @@ if work_dir.exists():
     shutil.rmtree(work_dir)
 work_dir.mkdir(exist_ok=True)
 
+corpus_output = Path(args.corpus_output)
+corpus_save_dir = Path(args.corpus_save_dir)
+corpus_save_dir.mkdir(exist_ok=True)
+
+def get_hash_file(filename):
+    h = hashlib.sha256()
+    b = bytearray(128*1024)
+    mv = memoryview(b)
+    with open(filename, 'rb', buffering=0) as f:
+        while n := f.readinto(mv):
+            h.update(mv[:n])
+    return h.hexdigest()
+
 try:
     subprocess.check_output(f'{centipede_bin} --binary={unicorn_bin} --workdir={work_dir} --num_runs={args.num_runs} --j={args.j}', shell=True, stderr=subprocess.STDOUT)
-    subprocess.check_output(f'python {transform_centipede_fuzz_results_to_silifuzz_corpus} --fuzzing_results="{work_dir}/corpus.*" --bin_dir="{silifuzz_bin_dir}" --corpus_output="{args.corpus_output}"', shell=True, stderr=subprocess.STDOUT)
+    subprocess.check_output(f'python {transform_centipede_fuzz_results_to_silifuzz_corpus} --fuzzing_results="{work_dir}/corpus.*" --bin_dir="{silifuzz_bin_dir}" --corpus_output="{corpus_output}"', shell=True, stderr=subprocess.STDOUT)
+    corpus_hash = get_hash_file(corpus_output)
+    corpus_save_file = corpus_save_dir / f'{corpus_hash}.corpus.xz'
+    shutil.copy(corpus_output, corpus_save_file)
 except Exception as e:
     panic(str(e))
 
